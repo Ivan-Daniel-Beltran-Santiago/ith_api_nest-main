d42aabfb2f29e4690f9af04597130b619105b6ca
Ivan-Daniel-Beltran-Santiago <starfoxfan950@hotmail.com>
2022-10-04 18:21:31 -0700

    Z

--- src/Users/user/user.service.ts
+++ src/Users/user/user.service.ts
@@ -1,4 +1,4 @@
-import { User } from './../../models/User';
+import { User } from './../../Models/user.model';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -13,7 +13,22 @@ export class UserService {
         return this.Users
     }
 
-    getByEmail(correo : string) : User{
-        return this.Users.find( (user) => user.correo === correo )
+    getByEmail(email : string) : User{
+        return this.Users.find( (user) => user.email === email )
     }
+
+    updateUserById(id: number, user: User): boolean {
+        let user_index = this.Users.findIndex((user) => user.id === id);
+        if (user_index !== -1) {
+          //mantener los datos que no se van a actualizar
+          this.Users[user_index] = {
+            id: user.id ?? this.Users[user_index].id,
+            username: user.username ?? this.Users[user_index].username,
+            email: user.email ?? this.Users[user_index].email,
+            cellphone: user.cellphone ?? this.Users[user_index].cellphone,
+          };
+          return true;
+        }
+        return false;
+      }
 }
